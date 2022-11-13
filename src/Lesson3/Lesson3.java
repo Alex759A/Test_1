32b476e2cdb1a304460c9cf58d624b4b847aa331
Alex759A <brelevskiyaleksey@gmail.com>
2022-11-13 17:21:49 +0600

    Brench  Download requests!!

--- src/Lesson3/Lesson3.java
+++ src/Lesson3/Lesson3.java
@@ -3,5 +3,6 @@ package Lesson3;
 public class Lesson3 {
     public static void main(String[] args) {
         System.out.println(" Lesson 3 new branch");
+        System.out.println(" Download requests!!");
     }
 }
